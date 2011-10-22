c110f623efc024fc24d8237f47013094f903aefb
Jim Kirkwood <kirkwojl@hotmail.com>
2011-10-22 15:32:19 -0400

    first piece of ordered recon

--- src/jdbcrecon/core.clj
+++ src/jdbcrecon/core.clj
@@ -1,12 +1,10 @@
 (ns jdbcrecon.core
   (:require [clojure.java.jdbc :as sql]))
 
-; TODO: build query
 (defn build-query
   "Uses the params to build the select query. 
   Expects to have :tblname, :keycols, :versioncol, and optional :querysuffix"
   [params]
-  ; TODO: need commas
   (.trim (str "SELECT " 
            (reduce #(str %1 "," %2) (:keycols params)) 
            ","
@@ -39,6 +37,45 @@
 ; TODO: memory-map compare recon
 ; TODO: touch entity
 ; TODO: exception-func implementations (log, jms, touch, system out)
+
+(defn tgt-behind?
+  "Returns true if the src row is ahead of the target row"
+  [src-row tgt-row]
+  (> (src-row 1) (tgt-row 1)))
+
+(defn src-behind?
+  "Returns true if the target row is ahead of the src row"
+  [src-row tgt-row]
+  (< (src-row 1) (tgt-row 1)))
+
+(defn issue-code
+  "Returns the issue keyword for the specified rows or nil if there is no issue"
+  [src-row tgt-row]
+  (cond
+    (tgt-behind? src-row tgt-row) :tgt-behind
+    (src-behind? src-row tgt-row) :src-behind
+    :else nil))
+
+(defn no-nil-cons
+  "Adds item to seq if it is not nil, otherwise returns seq"
+  [item seq]
+  (if (nil? item) seq (cons item seq)))
+
+(defn ordered-row-recon
+  "Reconciliation function that assumes the two sequences share the same ordering.
+  Reconciliation is applied as the sequences are consumed.  Useful where the tables are larger
+  and we don't want to load all the contents in memory as with other implementations.
+  Returns a sequence of [keymap <issue>] where issue is :tgt-behind :src-behind :tgt-missing :src-missing"
+  [src-seq tgt-seq]
+  ; start off assuming nothing can be missing - just checking versions
+  ; and both sequences are same size
+  (let [src-row (first src-seq)
+        tgt-row (first tgt-seq)]
+    (if (empty? src-seq) nil 
+      (lazy-seq 
+        (no-nil-cons 
+          (issue-code src-row tgt-row)
+          (ordered-row-recon (rest src-seq) (rest tgt-seq)))))))
 
 (defn reconcile
   "Executes a reconciliation.  source-params and target-params includes all connection parameters 
--- test/jdbcrecon/test/core.clj
+++ test/jdbcrecon/test/core.clj
@@ -21,3 +21,27 @@
          (build-query {:tblname "testtable"
                        :keycols ["key1" "key2" "key3"]
                        :versioncol "version"}))))
+
+(defn range-rows
+  ([n] (range-rows 0 n 0))
+  ([s n] (range-rows s n 0))
+  ([s n v] (map #(vector {"key1" %1 "key2" %1} (+ v %1)) (range s n))))
+
+(deftest test-ordered-row-recon-no-issues
+  (let [src-rows (range-rows 10)
+        tgt-rows (range-rows 10)]
+    (is (= (count (ordered-row-recon src-rows tgt-rows)) 0))))
+
+(deftest test-ordered-row-recon-target-behind
+  (let [src-rows (range-rows 0 10 1)
+        tgt-rows (range-rows 0 10)
+        result (ordered-row-recon src-rows tgt-rows)]
+    (is (= (count result) 10))
+    (is (= (count (filter #(= :tgt-behind %1) result)) 10))))
+
+(deftest test-ordered-row-recon-source-behind
+  (let [src-rows (range-rows 0 10)
+        tgt-rows (range-rows 0 10 1)
+        result (ordered-row-recon src-rows tgt-rows)]
+    (is (= (count result) 10))
+    (is (= (count (filter #(= :src-behind %1) result)) 10))))
