02e3898c91115e6c7dba960d7a82c8504b7df48b
James Kirkwood <kirkwojl@hotmail.com>
2011-10-22 11:20:06 -0400

    initial checkin of project shell

---
+++ src/jdbcrecon/core.clj
@@ -0,0 +1,1 @@
+(ns jdbcrecon.core)
---
+++ test/jdbcrecon/test/core.clj
@@ -0,0 +1,6 @@
+(ns jdbcrecon.test.core
+  (:use [jdbcrecon.core])
+  (:use [clojure.test]))
+
+(deftest replace-me ;; FIXME: write
+  (is false "No tests have been written."))
