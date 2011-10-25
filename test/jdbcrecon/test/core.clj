01b4df76f212d37246230673ea1539feb91ab7ca
Jim Kirkwood <kirkwojl@hotmail.com>
2011-10-25 18:40:28 -0400

    added test for in-memory-recon

--- test/jdbcrecon/test/core.clj
+++ test/jdbcrecon/test/core.clj
@@ -61,3 +61,13 @@
     (is (= (count result) 1))
     (is (= ((first result) 1) :version-mismatch))
     (is (= ((first result) 0) {"key1" 1 "key2" 1}))))
+
+(deftest test-in-memory-recon
+  (let [src-rows (range-rows 0 10)
+        tgt-rows (range-rows 0 10)
+        missing-row (range-rows 0 9)
+        mismatch-row (range-rows 0 10 1)]
+    (is (empty? (in-memory-recon src-rows tgt-rows)))
+    (is (= [{"key1" 9 "key2" 9} :tgt-missing] (first (in-memory-recon src-rows missing-row))))
+    (is (= [{"key1" 9 "key2" 9} :src-missing] (first (in-memory-recon missing-row src-rows))))
+    (is (= (count (in-memory-recon src-rows mismatch-row)) 10))))
