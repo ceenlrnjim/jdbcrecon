69b379411f92909ac232fa9690522e9ea52c7803
Jim Kirkwood <kirkwojl@hotmail.com>
2011-10-28 19:19:11 -0400

    added logging libraries and dependencies

--- project.clj
+++ project.clj
@@ -1,5 +1,7 @@
 (defproject jdbcrecon "1.0.0-SNAPSHOT"
   :description "FIXME: write description"
   :dependencies [[org.clojure/clojure "1.3.0"]
+                 [org.clojure/tools.logging "0.1.0"]
                  [org.clojure/java.jdbc "0.1.0"]
+                 [log4j/log4j "1.2.16"]
                  [mysql/mysql-connector-java "5.1.18"]])
